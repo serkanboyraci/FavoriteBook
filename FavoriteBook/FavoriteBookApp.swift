a9bcd662a92908a114f269c61c15fdd7ac8ea165
serkanboyraci <serkanboyraci@gmail.com>
2022-12-01 23:33:51 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Ali serkan Boyracı  on 1.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
