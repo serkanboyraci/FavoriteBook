cfa1ae542b6e7c428bbd670e1dbbd8a5cbafc121
serkanboyraci <serkanboyraci@gmail.com>
2022-12-02 21:39:00 +0300

    adding NavigationView and DetailsView

--- FavoriteBook/ContentView.swift
+++ FavoriteBook/ContentView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List { // to show as a list
-            ForEach(myFavorites) { favorite in // like for loop
-                Section(header: Text(favorite.title)) { //to show as a section
-                    ForEach(favorite.elements) { element in // again like a for loop
-                        Text(element.name)
+        
+        NavigationView{
+            
+            List { // to show as a list
+                ForEach(myFavorites) { favorite in // like for loop
+                    Section(header: Text(favorite.title)) { //to show as a section
+                        ForEach(favorite.elements) { element in // again like a for loop
+                            NavigationLink(destination: DetailsView()) { // to go to the DatailsView.
+                                //
+                                Text(element.name) // if we dont add NavigationView, we cant click elements and see them in blurred.
+                                
+                            }
+                        }
                     }
                 }
-            }
+            }.navigationBarTitle(Text("Favorite Book")) // add list curly braces.
         }
-        
-  
     }
 }
 
